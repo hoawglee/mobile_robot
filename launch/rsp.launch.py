4cfda9729de34b5fff25fa71baa295e5d16a278a
hoawglee <nhatka154@gmail.com>
2026-04-04 23:51:48 +0700

    Motion in Gazebo

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,13 +1,13 @@
-import os
+import os #xu ly duong dan file
 
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory #lay duong dan toi package mobile_robot
 
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
-import xacro
+import xacro #convert file xacro sang urdf
 
 
 def generate_launch_description():
@@ -16,12 +16,12 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('mobile_robot'))
+    pkg_path = os.path.join(get_package_share_directory('mobile_robot')) # lay folder package mobile_robot
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file).toxml()
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time} #truyen du lieu robot vao node 
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -39,3 +39,4 @@ def generate_launch_description():
 
         node_robot_state_publisher
     ])
+# lay file robot cua minh bien no thanh URDF dua cho ROS publish de RViz nhin thay 
